2d894664619bdc21b248ba2b54a27ca61825a899
Vladimir Gordeev <gordeev.vladimir.v@gmail.com>
2020-11-08 20:11:06 +0300

    add http api that creates new cards

--- backend/src/http_server.js
+++ backend/src/http_server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 import { HTTP_PORT } from './config.js';
 
@@ -6,6 +7,7 @@ import { HTTP_PORT } from './config.js';
 // API for ExpressJS, for consistency
 export default function startHttpServer(sequelize) {
   const app = express();
+  app.use(bodyParser());
 
   // this request returns all cards that we have in database
   app.get('/cards.json', async (req, res) => {
@@ -18,6 +20,25 @@ export default function startHttpServer(sequelize) {
     // console.log(JSON.stringify(jsonCards));
     res.type('json');
     res.send(JSON.stringify(jsonCards));
+  });
+
+  // can be tested from console this way:
+  // curl -X POST -H "Content-Type: application/json" -d '{"sections": [{"russianText": "Меня зовут…","phoneticText": "[minya zavoot]","englishText": "My name is…"},{"russianText": "Как вас зовут?","phoneticText": "[kak vas zavoot]","englishText": "What is your name? (formal, when speaking to an older person)"}]}' http://localhost:3000/cards.json
+  app.post('/cards.json', async (req, res) => {
+    const card = await sequelize.models.Card.create({});
+    const { sections } = req.body;
+
+    for (const section of sections) {
+      const { englishText, phoneticText, russianText } = section;
+      await sequelize.models.Section.create({
+        englishText,
+        russianText,
+        phoneticText,
+        CardId: card.id,
+      });
+    }
+
+    res.send(JSON.stringify({ id: card.id }));
   });
 
   app.listen(HTTP_PORT, () => {
